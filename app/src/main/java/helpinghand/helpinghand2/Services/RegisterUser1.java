40f3baa481b8818b55f11fe67cf5ac07ca465999
suman <adhikaris304@gmail.com>
2019-06-24 08:20:02 +0545

    Add RegisterUser1 for user in forUser Branch

--- app/src/main/java/helpinghand/helpinghand2/Services/RegisterUser1.java
+++ app/src/main/java/helpinghand/helpinghand2/Services/RegisterUser1.java
@@ -11,8 +11,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import helpinghand.helpinghand2.forUser.User;
-
 /**
  * Created by Rasil10 on 2017-05-26.
  */

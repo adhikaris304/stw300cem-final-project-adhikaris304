8fe16b9619a3907739b413663c5daf67d602f837
suman <adhikaris304@gmail.com>
2019-06-30 08:17:44 +0545

    Add User Activity in the project

---
+++ app/src/main/java/helpinghand/helpinghand2/forUser/User.java
@@ -0,0 +1,79 @@
+package helpinghand.helpinghand2.forUser;
+
+/**
+ * Created by Rasil10 on 2017-05-26.
+ */
+
+public class User {
+    String id,name,phone,address,password,email,role;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public User(String id, String name, String address, String email, String phone, String password, String role) {
+       this.id=id;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.password = password;
+        this.email = email;
+        this.role=role;
+    }
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
